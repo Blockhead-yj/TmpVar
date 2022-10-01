6a2c65f664b385ee176371fcbd4312851af0242f
Blockhead-yj <136271877@qq.com>
2022-10-01 08:43:54 +0800

    finish v0.0.1

---
+++ TempEnv/main.py
@@ -0,0 +1,71 @@
+from copy import deepcopy
+from inspect import ismodule
+class TemporalEnv:
+    def __init__(self, vars_dict:dict={}, /, **kwargs) -> None:
+        self.vars = vars_dict | kwargs
+        self.env = globals()
+        self.env_backup = {k:deepcopy(v) for k,v in self.env.items() if not k.startswith('_') and not ismodule(v)}
+
+    def deep_update(self):
+        for k,v in self.vars.items():
+            varname = TemporalEnv.parse_properties(k)
+            if len(varname) == 1:
+                self.env[k] = v
+            else:
+                try:
+                    inst = self.env[varname[0]]
+                except KeyError:
+                    raise ValueError(f'Instance {varname[0]} is not in current environment, please check if there is a spelling error.')
+                for property in varname[1:-1]:
+                    try:
+                        inst = getattr(inst, property)
+                    except AttributeError:
+                        raise AttributeError(f'Instance {inst} dose not have attribute {property}')
+                setattr(inst, varname[-1], v)
+                    
+    @staticmethod
+    def parse_properties(varname:str):
+        if '.' in varname:
+            return tuple(varname.split('.'))
+        else:
+            return (varname,)
+
+    def __enter__(self):
+        self.deep_update()
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for k,v in self.vars.items():
+            varname = TemporalEnv.parse_properties(k)
+            if len(varname) == 1:
+                try:
+                    self.env[k] = self.env_backup[k]
+                except KeyError:
+                    self.env.pop(k)
+            else:
+                self.env[varname[0]] = self.env_backup[varname[0]]
+                
+
+class TestClass:
+    def __init__(self, a, b, c) -> None:
+        self.a = a
+        self.b = b
+        self.c = c
+
+if __name__ == '__main__':
+    a = 3
+    b = 6
+    c = 9
+    d = TestClass(a, b ,c)
+
+    with TemporalEnv({'d.a':3354}, a=453):
+        print('临时环境：——————')
+        print(a)
+        print(b)
+        print(c)
+        print(d.a)
+    print('原始环境：——————')
+    print(a)
+    print(b)
+    print(c)
+    print(d.a)
